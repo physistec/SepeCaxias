0ddaa4d53cc0d9c6283f62a575462d00e9edbe3e
Rodinei Costa <rodineicosta@physistec.com.br>
2017-08-22 11:49:55 -0300

    Logotipo de topo responsivo

--- header.php
+++ header.php
@@ -102,7 +102,7 @@
 			<?php if ( get_header_image() ) : ?>
 			<div id="site-header">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-					<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+					<img class="img-responsive" src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
 				</a>
 			</div>
 			<?php else: ?>
